27fe0a0262ba08faae1ca3273a77f0b9e3769425
Marc Blais <marcantblais@gmail.com>
2022-10-10 15:29:52 -0400

    init commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,11 @@
 import React from "react"
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 
-const test = (
-    <div>
-        <h1>App connected</h1>
-        <p>Main Page</p>
+const message = (
+    <div className="construction-en-cours">
+        <h1>Construction en cours</h1>
+        <p>Disponible dès le 15 octobre</p>
+        <img src="https://www.pngkey.com/png/detail/187-1874320_construction-cone-png-traffic-cones-clipart.png" alt="construction cone" />
     </div>
 )
 
@@ -13,7 +14,7 @@ const App = () => {
         <div className="container">
             <BrowserRouter>
                 <Routes>
-                    <Route path="/" exact element={test} />
+                    <Route path="/" exact element={message} />
                 </Routes>
             </BrowserRouter>
         </div>
