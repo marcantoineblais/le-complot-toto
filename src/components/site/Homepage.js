a2b48f0a37a25c2a686a3847a13a06e559a81e3e
Marc Blais <marcantblais@gmail.com>
2022-10-12 17:56:29 -0400

    added colored borders to homepage, fixed some mobile display issues and updated scss variables

--- src/components/site/Homepage.js
+++ src/components/site/Homepage.js
@@ -1,9 +1,32 @@
-import React from "react"
+import React, { useEffect, useRef, useState } from "react"
+import { wait } from "../../helpers"
 
 const Homepage = () => {
+  
+  const [colorIndex, setColorIndex] = useState(0)
+  const homepageRef = useRef()
+  
+  useEffect(() => {
+    const borderColors = ['yellow', 'red', 'green']
+    const colors = {
+      red: 'rgb(150, 0, 0)',
+      yellow: 'rgb(255, 243, 80)',
+      green: 'rgb(0, 199, 0)'
+    }
+
+    const animate = async () => {
+      homepageRef.current.style.border = `10px solid ${colors[borderColors[colorIndex]]}`
+      await wait(1000)
+      setColorIndex((colorIndex + 1) % borderColors.length)
+    }
+
+    animate()
+
+  }, [colorIndex])
+    
   return (
-    <div className="homepage">
-      CONNECTED
+    <div ref={homepageRef} className="homepage">
+      <h1>BIENTOT DISPONIBLE</h1>
     </div>
   )
 }
