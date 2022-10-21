5e5c7233ebbd3a5be0c454167e55bcfd20d33f2c
Marc Blais <marcantblais@gmail.com>
2022-10-21 05:35:53 -0400

    removed iframe border and window-container since it was not needed anymore

--- src/components/server/Window.js
+++ src/components/server/Window.js
@@ -3,17 +3,15 @@ import React from "react"
 const Window = ({ content, setActiveWindow }) => {
 
   return (
-    <div className="window-container">
-      <div className="window">
-        <div className="bar">
-          <h3>{content.title}</h3>
-          <button onClick={() => setActiveWindow(null)}>&times;</button>
-        </div>
-        <div className="content">
-          {content.image ? <img src={content.image} alt={content.alt} /> : null}
-          {content.component ? content.component : null}
-          {content.description ? <p>{content.description}</p> : null}
-        </div>
+    <div className="window">
+      <div className="bar">
+        <h3>{content.title}</h3>
+        <button onClick={() => setActiveWindow(null)}>&times;</button>
+      </div>
+      <div className="content">
+        {content.image ? <img src={content.image} alt={content.alt} /> : null}
+        {content.component ? content.component : null}
+        {content.description ? <p>{content.description}</p> : null}
       </div>
     </div>
   )
